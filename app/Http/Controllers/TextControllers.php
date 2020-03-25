704377a4f1bfe494bba5c30b46eaf1f5ed7d9d2c
hakhengou <hakhengou12@gmail.com>
2020-03-25 11:13:56 -0700

    Delete Model

--- app/Http/Controllers/TextControllers.php
+++ app/Http/Controllers/TextControllers.php
@@ -4,41 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-use App\Text;
 use Storage;
 class TextControllers extends Controller
 {
     public function create(Request $req)
     {
-        $stu = new Text();
         $fileName= 'text.txt';
-        // $k = 0;
-        // while(!$fileName){
-        //     if(!file_exists("text$k.txt"))
-        //         $fileName = "text$k.txt";
-        //     $k++;
-        // }
-        $locationsVoice= 'http://127.0.0.1:8000/medias/vivok.mp3';
         $myfile = fopen($fileName, "w") or die("Unable to open file!");
         $txt = $req->input('text');
         fwrite($myfile, $txt);
         fclose($myfile);
-
         #Run Scirpt Python
         system("testScript.py");
-
         #Run Script Sh
         system("testScript.sh");
-
-        $stu->pathVoice=$locationsVoice;
-        // $stu->save();
+        $stu= "Sucess Save Text";
         return response()->json($stu);
     }
     public function read()
     {
-        $stu = Text::all();
-        $data = array('status'=>'success','data'=>$stu);
-       return response()->json($data);
+        $data = ['pathVoice'=>'http://127.0.0.1:8000/medias/vivok.mp3'];
+        return response()->json($data);
     }
 }
